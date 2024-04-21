41999a262c0c6bd8df03b6bfc3d30be1acdac435
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-04-21 17:32:57 -0300

    build: config pandacss

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  cwd: 'src',
+  include: ['./components/*.{js,jsx,ts,tsx}', './app/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
@@ -16,5 +17,5 @@ export default defineConfig({
   },
 
   // The output directory for your css system
-  outdir: "styled-system",
+  outdir: 'styles',
 });
