d768602a47846ca95f4632c3578ab255ba40eb3c
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-04-22 15:50:44 -0300

    fix(panda): set correct paths for declarations

--- panda.config.ts
+++ panda.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from '@pandacss/dev';
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
+  jsxFramework: 'react',
 
   // Where to look for your css declarations
   include: [
-    './src/components/*.{js,jsx,ts,tsx}',
-    './src/app/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/app/**/*.{js,jsx,ts,tsx}',
   ],
   // Path configuration
   outdir: 'styled-system',
