2cf2a6d5eafb0a458489291504065fdf1d9d2c57
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-04-28 12:58:47 -0300

    feat: create and export body for the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>Hello world!</main>;
-}
+'use server';
+import { Activity } from '@/components/graphs/activity';
+import { getLastYearData } from '@/services/db/data';
+
+export const Body = async () => {
+  const data = await getLastYearData(1);
+
+  return (
+    <>
+      <h1>Last year&apos;s data</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <Activity records={data}></Activity>
+    </>
+  );
+};
+
+const Home = async () => {
+  return (
+    <div>
+      <Body></Body>
+    </div>
+  );
+};
+
+export default Home;
