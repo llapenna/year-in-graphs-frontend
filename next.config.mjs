14aefeeef6ba7cd788ded25d5858ad2b75be0fdf
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-05-02 15:58:49 -0300

    feat: add authentication redirects depending on cookies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/not-authorized',
+        destination: '/',
+        permanent: false,
+        has: [{ type: 'cookie', key: 'userId' }],
+      },
+      {
+        source: '/',
+        destination: '/not-authorized',
+        permanent: false,
+        missing: [{ type: 'cookie', key: 'userId' }],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
